aca82fe569aceb2a5d40086d735ec66995805c96
dragonryan06 <dragonryan06@gmail.com>
2024-10-26 15:52:36 -0400

    Remove the godawful camera

--- entities/asteroid.gd
+++ entities/asteroid.gd
@@ -1,4 +1,4 @@
-extends Grid
+extends TileMapLayer
 
 # Called when the node enters the scene tree for the first time.
 func generate():
--- systems/grid.gd
+++
@@ -1,4 +0,0 @@
-extends TileMapLayer
-class_name Grid
-
-## A Grid object is a TileMapLayer that the player is able to stand and walk on.
