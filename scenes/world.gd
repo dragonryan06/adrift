6c3aff072c4233ba6f3a7bca8b8ca22b5c7ef92d
dragonryan06 <dragonryan06@gmail.com>
2024-10-26 17:07:12 -0400

    Insert that picture of the ant with the bindle

--- scenes/world.gd
+++ scenes/world.gd
@@ -1,37 +1,16 @@
 extends Node
 
-const asteroid_count = 100
-
-@onready var Asteroid = preload("res://entities/asteroid.tscn")
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# make a bunch of bounding boxes
-	for a in asteroid_count:
-		var area = Area2D.new()
-		area.position = Vector2(randi_range(-5000,5000),randi_range(-5000,5000))
-		area.rotation = randf_range(0,-PI)
-		var collider = CollisionShape2D.new()
-		var shape = RectangleShape2D.new()
-		var size = randf_range(100.0,800.0)
-		shape.size = Vector2(size,size)
-		collider.shape = shape
-		area.add_child(collider)
-		$Asteroids.add_child(area)
+	var noise = FastNoiseLite.new()
+	noise.seed = randi()
+	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
+	noise.frequency = 0.2
+	noise.fractal_octaves = 1
+	noise.fractal_gain = 1
 	
-	# kill overlapping bounding boxes
-	for c:Area2D in $Asteroids.get_children():
-		if len(c.get_overlapping_areas()) > 0:
-			$Asteroids.remove_child(c)
-			c.queue_free()
-	
-	# generate asteroids in these locations
-	for c in $Asteroids.get_children():
-		if c is Area2D:
-			var a = Asteroid.instantiate()
-			$Asteroids.add_child(a)
-			a.position = c.position
-			a.rotation = c.rotation
-			a.generate()
-			
-			$Asteroids.remove_child(c)
-			c.queue_free()
+	for x in 1024:
+		for y in 1024:
+			var sample = noise.get_noise_2d(x,y)
+			if sample > 0.25:
+				$Asteroids.set_cell(Vector2i(x,y),0,Vector2i(0,0))
+				$Asteroids/AsteroidWalls.set_cell(Vector2i(x,y),0,Vector2i(0,0))
