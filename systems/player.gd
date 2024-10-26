5d3e2b79fa255def9a5b771bc2ff9da67723d453
dragonryan06 <71857681+dragonryan06@users.noreply.github.com>
2024-10-26 16:07:12 -0400

    Revert "Early asteroid generation"

--- systems/player.gd
+++ systems/player.gd
@@ -3,20 +3,27 @@ extends CharacterBody2D
 @export var HEAD_TURN_SPEED = 0.5
 @export var MOVE_SPEED = 250
 
+var parent_grid = null
+
 func _physics_process(delta:float) -> void:
 	var angle = global_position.angle_to_point(get_global_mouse_position())+PI/2.0
 	rotation = lerp_angle(rotation,angle,HEAD_TURN_SPEED)
-	# This is so the camera can rotate independently of its parent
+	# This is so the camera can rotated independently of its parent
 	$Pivot.rotation = -rotation
 	
 	var movement = Vector2(Input.get_axis("move_left","move_right"), Input.get_axis("move_up","move_down"))
+	if is_instance_valid(parent_grid):
+		$Pivot.rotation += parent_grid.rotation
+		movement = movement.rotated(parent_grid.rotation)
 	
 	if movement.x:
 		velocity.x = movement.x*MOVE_SPEED
-	elif $GridDetector.is_colliding():
+	elif is_instance_valid(parent_grid):
 		velocity.x = move_toward(velocity.x, 0, MOVE_SPEED)
 	if movement.y:
 		velocity.y = movement.y*MOVE_SPEED
-	elif $GridDetector.is_colliding():
+	elif is_instance_valid(parent_grid):
 		velocity.y = move_toward(velocity.y, 0, MOVE_SPEED)
 	move_and_slide()
+	
+	parent_grid = $GridDetector.get_collider()
